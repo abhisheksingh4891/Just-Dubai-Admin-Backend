5587f0d9a92d7f70efa9983db5dad2e35b75d1c1
Abhishek Singh <singh2811abhishek@gmail.com>
2024-06-25 11:20:25 +0530

    commit

--- Controller/AdminController.js
+++ Controller/AdminController.js
@@ -1,5 +1,5 @@
 const jwt = require("jsonwebtoken");
-const bcrypt = require("bcrypt");
+const bcrypt = require('bcryptjs');
 const AdminModel = require("../Models/AdminModel");
 const multer = require("multer");
 const path = require("path");
